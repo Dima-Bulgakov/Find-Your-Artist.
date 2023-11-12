502bf1dffdc08befd04585e5e0bdb2ef906fd1e7
Dima-Bulgakov <bulgakovdu@gmail.com>
2023-11-12 13:57:33 +0200

    Initial Commit

---
+++ Aldebaran/AldebaranApp.swift
@@ -0,0 +1,17 @@
+//
+//  AldebaranApp.swift
+//  Aldebaran
+//
+//  Created by Dima on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AldebaranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
